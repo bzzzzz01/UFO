e1c203b7b183cd67eec84e5a3d09589af30fe8aa
bzzzzz01 <905161357@qq.com>
2018-04-17 23:53:32 +0800

    Add files via upload

---
+++ homework4/UFOActionManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOActionManager : ActionManager, ActionCallback
+{
+    public float slowSpeed = 3;
+    public float middleSpeed = 5;
+    public float fastSpeed = 10;
+    public FirstSceneController sceneController;
+    public FlyAction slowFlyAction, middleFlyAction, fastFlyAction;
+
+    protected new void Start()
+    {
+        sceneController = Director.getInstance().currentSceneController as FirstSceneController;
+        sceneController.ufoActionManager = this;
+        slowFlyAction = FlyAction.getAction(new Vector3(Random.Range(-1, 1f), Random.Range(-1, 1f)), slowSpeed);
+        middleFlyAction = FlyAction.getAction(new Vector3(Random.Range(-1, 1f), Random.Range(-1, 1f)), middleSpeed);
+        fastFlyAction = FlyAction.getAction(new Vector3(Random.Range(-1, 1f), Random.Range(-1, 1f)), fastSpeed);
+
+    }
+
+    protected new void Update()
+    {
+        base.Update();
+    }
+
+    public void ActionEvent(Action source, GameObject gameObjectParam = null, ActionEventType events = ActionEventType.Compeleted,
+                            int intParam = 0, string strParam = null)
+    {
+        if (events == ActionEventType.Started)
+        {
+            slowFlyAction = FlyAction.getAction(new Vector3(Random.Range(-1, 1f), Random.Range(-1, 1f)), slowSpeed);
+            middleFlyAction = FlyAction.getAction(new Vector3(Random.Range(-1, 1f), Random.Range(-1, 1f)), middleSpeed);
+            fastFlyAction = FlyAction.getAction(new Vector3(Random.Range(-1, 1f), Random.Range(-1, 1f)), fastSpeed);
+            this.RunAction(gameObjectParam, source, this);
+        }
+        else if(events == ActionEventType.Compeleted)
+        {
+            UFOFactory ufofactory = Singleton<UFOFactory>.Instance;
+            ufofactory.freeUFO(gameObjectParam);
+        }
+
+    }
+
+}
---
+++ homework4/UFOFactory.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOFactory : MonoBehaviour
+{
+
+    public GameObject UFOPrefab = null;
+
+    private List<UFO> used = new List<UFO>();
+    private List<UFO> free = new List<UFO>();
+
+    private void Awake()
+    {
+        UFOPrefab = Instantiate(Resources.Load("UFO") as GameObject);
+        UFOPrefab.SetActive(false);
+    }
+
+    public GameObject getUFO(int round)
+    {
+        GameObject newUFO = null;
+        if (free.Count > 0)
+        {
+            newUFO = free[0].gameObject;
+            newUFO.SetActive(true);
+            free.Remove(free[0]);
+        }
+        else
+        {
+            newUFO = Instantiate(UFOPrefab);
+            newUFO.SetActive(true);
+            newUFO.AddComponent<UFO>();
+        }
+        used.Add(newUFO.GetComponent<UFO>());
+
+        if (round == 1)
+        {
+            newUFO.GetComponent<UFO>().size = new Vector3(1.5f, 1.5f, 1.5f);
+            newUFO.GetComponent<UFO>().color = Color.red;
+            newUFO.GetComponent<Transform>().localScale= new Vector3(1.5f, 1.5f * 0.5f, 1.5f);
+            newUFO.GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (round == 2)
+        {
+            newUFO.GetComponent<UFO>().size = new Vector3(1, 1, 1);
+            newUFO.GetComponent<UFO>().color = Color.green;
+            newUFO.GetComponent<Renderer>().material.color = Color.green;
+        }
+        if (round == 3)
+        {
+            newUFO.GetComponent<UFO>().size = new Vector3(0.8f, 0.8f, 0.8f);
+            newUFO.GetComponent<UFO>().color = Color.blue;
+            newUFO.GetComponent<Transform>().localScale = new Vector3(0.8f, 0.8f * 0.5f, 0.8f);
+            newUFO.GetComponent<Renderer>().material.color = Color.blue;
+        }
+        return newUFO;
+    }
+
+    public void freeUFO(GameObject theUFO)
+    {
+        UFO tmp = null;
+        foreach (UFO ufo in used)
+        {
+            if (ufo.gameObject.GetInstanceID() == theUFO.GetInstanceID() )
+            {
+                tmp = ufo;
+            }
+        }
+        if (tmp != null)
+        {
+            tmp.gameObject.SetActive(false);
+            free.Add(tmp);
+            used.Remove(tmp);
+        }
+    }
+
+}
